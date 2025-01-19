5a96c926844f4bd7bb009f02b6f7431c0edf2da1
Masud Rana <mr.masudrana00@gmail.com>
2025-01-20 03:48:56 +0600

    Generate build files

--- build/posts-grid/index.asset.php
+++ build/posts-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'cf60c28fce19be795f41');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '2ce732684bbbba434b4c');
--- build/testimonial/view.asset.php
+++ build/testimonial/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'cc44e3dfa861f604c61e');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
